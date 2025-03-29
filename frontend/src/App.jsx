2a28c5af378620db45253de689bce505665b7217
Carmencc <carmencai5114@gmail.com>
2025-03-28 22:43:15 -0400

    Navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { Navbar } from "./components/navbar";
 import "./layout.scss";
 
 function App() {
-  return <div className="layout"></div>;
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./navbar.scss";
+
+export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a className="logo" href="/">
+          <img src="./logo.png" alt="" />
+          <span>CarmEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="btnRegister">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
