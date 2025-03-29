791dd12712b9033249f6d1a039feca471bc37359
Carmencc <carmencai5114@gmail.com>
2025-03-29 17:52:48 -0400

    ListPage

---
+++ frontend/src/components/Map/Map.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Map.scss";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Pin } from "../Pin/Pin";
+
+export const Map = ({ items }) => {
+  const position = [51.505, -0.09];
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={7}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin key={item.id} item={item} />
+      ))}
+    </MapContainer>
+  );
+};
--- frontend/src/routes/ListPage/ListPage.jsx
+++ frontend/src/routes/ListPage/ListPage.jsx
@@ -1,5 +1,27 @@
+import { Filter } from "../../components/Filter/Filter";
+import { Card } from "../../components/Card/Card";
+import { listData } from "../../lib/dummydata";
 import "./ListPage.scss";
+import { Map } from "../../components/Map/Map";
 
 export const ListPage = () => {
-  return <div>ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          <div className="cards">
+            {data.map((item) => (
+              <Card key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
+    </div>
+  );
 };
